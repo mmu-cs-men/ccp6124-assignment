aa6e4c4c5b38b287674df0f0f24c904b4598f35b
Laxman Pillai <laxman.pillai1104@gmail.com>
2024-06-01 21:51:58 +0800

    Make SplashScreen

---
+++ util/SplashScreen.cpp
@@ -0,0 +1,52 @@
+#include "SplashScreen.h"
+#include "../robots/base/Robot.h"
+#include "ConfigurationParser.h"
+#include <iomanip>
+#include <iostream>
+
+void SplashScreen::display(int xDim, int yDim, int maxSteps, int robotNum,
+                           DynamicArray<std::shared_ptr<Robot>> robots)
+{   system("cls");
+
+    std::string star(100, '*');
+    std::string line(70, '-');
+
+    std::cout << std::endl;
+
+    std::cout << "Welcome to Robot War Simulator.This is a game made for our "
+                 "CPP6124 assignment\n";
+
+    std::cout << "Based on the configuration file, the following settings will "
+                 "be applied to your simulation\n\n";
+
+    std::cout << star << std::endl;
+
+    std::cout << "\nBattlefield Dimensions: " << xDim << "x" << yDim
+              << std::endl;
+    std::cout << "Steps: " << maxSteps << std::endl;
+    std::cout << "Robots: " << robotNum << std::endl << std::endl;
+
+    std::cout << "Robot List:" << std::endl;
+
+    std::cout << line << std::endl;
+
+    std::cout <<"NAME" 
+              << std::setw(18) << std::right <<"TYPE"
+              << std::setw(32) << "POSITION (x,y)" 
+              << std::setw(12) << "SYMBOL\n";
+
+    std::cout << line << std::endl;
+
+     
+
+    for (int i = 0; i < robots.size(); ++i)
+    {
+        auto robot = robots[i];
+        std::cout << std::left << std::setw(18) << robot->getName()
+                  << std::setw(22) <<"*******************"
+                  << std::setw(19) << ("(" + std::to_string(robot->getXPos()) + "," +std::to_string(robot->getYPos()) + ")")
+                  << robot->getSymbol() << "\n";
+    }
+    
+    std::cout<<"\n"<<star<<std::endl;
+}
