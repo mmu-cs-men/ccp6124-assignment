c54ad789a47e50af965742e24bbc65dc0f186f7f
Harris Majeed <harrismajeed2004@gmail.com>
2024-06-02 12:15:03 +0800

    add documentation

--- util/Helper.h
+++ util/Helper.h
@@ -30,8 +30,28 @@ struct Helper
          */
         static int generateRandomNumber(int a, int b);
 
+        /**
+         * @brief Appends a string to the log file.
+         *
+         * This method appends the given string to the log file. If the log
+         * count is zero, it updates the log count first. The log file is named
+         * based on the current log count (how many log files exist already) and
+         * is located in the "logs" directory.
+         *
+         * @param str The string to append to the log file.
+         *
+         * @throws std::runtime_error if the file cannot be opened.
+         */
         static void appendStrToLogFile(std::string str);
 
+        /**
+         * @brief Updates the log count based on existing log files.
+         *
+         * This method creates the "logs" directory if it does not exist and
+         * iterates through the log files in the directory to determine the
+         * highest log number. The log count is then set to one more than the
+         * highest log number.
+         */
         static void updateLogCount();
 
         static int logCount;
