1dddea41092fa58fa3cafd02edf65636cfa8fdc7
Harris Majeed <harrismajeed2004@gmail.com>
2024-06-02 14:54:47 +0800

    add credits: dullah

--- robots/base/ShootingRobot.cpp
+++ robots/base/ShootingRobot.cpp
@@ -1,3 +1,15 @@
+/**********|**********|**********|
+Program: ShootingRobot.cpp
+Course: Object Oriented Programming and Data Structures
+Trimester: 2410
+Name: Hawash, Abdullah
+ID: 1221102209
+Lecture Section: TC1L
+Tutorial Section: TT1L
+Email: 1221102209@student.mmu.edu.my
+Phone: 017-250-3208
+**********|**********|**********/
+
 #include "ShootingRobot.h"
 #include "../../util/Battlefield.h"
 #include <stdexcept>
